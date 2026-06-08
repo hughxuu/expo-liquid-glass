54a9828cde0eb7462571eb0da0a104062f37071f
xuch <xuzhanghui@jxzjun.cn>
2026-06-08 17:24:17 +0800

    ios tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,34 @@
-import { Stack } from 'expo-router'
+import { Badge, Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs'
+import { DynamicColorIOS } from 'react-native'
 
-export default function RootLayout() {
+export default function TabLayout() {
 	return (
-		<Stack
-			screenOptions={{
-				headerLargeTitle: true,
-				headerShadowVisible: false,
-				headerTransparent: true
-			}}>
-			<Stack.Screen
-				name='index'
-				options={{ title: '首页' }}
-			/>
-			<Stack.Screen
-				name='page'
-				options={{ title: '第二页' }}
-			/>
-		</Stack>
+		<NativeTabs
+			labelStyle={{
+				color: DynamicColorIOS({
+					dark: 'white',
+					light: 'black'
+				})
+			}}
+			tintColor={DynamicColorIOS({
+				dark: 'white',
+				light: 'black'
+			})}>
+			<NativeTabs.Trigger name='index' disablePopToTop>
+				<Label>Home</Label>
+				<Icon sf={{ default: 'house', selected: 'house.fill' }} drawable='custom_android_drawable' />
+				<Badge>3</Badge>
+			</NativeTabs.Trigger>
+
+			<NativeTabs.Trigger name='settings'>
+				<Icon sf='gear' drawable='custom_settings_drawable' />
+				<Label hidden>Settings</Label>
+				<Badge />
+			</NativeTabs.Trigger>
+
+			<NativeTabs.Trigger name='search' role='search'>
+				<Label>Search</Label>
+			</NativeTabs.Trigger>
+		</NativeTabs>
 	)
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,5 @@
-import { Link } from 'expo-router'
-import { Button, ScrollView, Text } from 'react-native'
+import { Text } from 'react-native'
 
 export default function index() {
-	return (
-		<ScrollView contentInsetAdjustmentBehavior='automatic'>
-			<Text>hello expo</Text>
-			<Link href='/page' asChild>
-				<Button title='Go Page' />
-			</Link>
-		</ScrollView>
-	)
+	return <Text>hello expo</Text>
 }
