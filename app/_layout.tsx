12ba950b6a14f81ece2f1e28dff3ddfda6e4b273
xuch <xuzhanghui@jxzjun.cn>
2026-06-08 16:49:46 +0800

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,21 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Stack } from 'expo-router'
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+	return (
+		<Stack
+			screenOptions={{
+				headerLargeTitle: true,
+				headerShadowVisible: false,
+				headerTransparent: true
+			}}>
+			<Stack.Screen
+				name='index'
+				options={{ title: '首页' }}
+			/>
+			<Stack.Screen
+				name='page'
+				options={{ title: '第二页' }}
+			/>
+		</Stack>
+	)
 }
